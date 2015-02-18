ba14e5eb8cbff15255747c96f023f2507482349a
Andrei Khveras <Andrei_Khveras@epam.com>
2015-02-18 15:02:44 +0300

    Update RestartingProxyTest.java

--- littleproxy-test/src/main/java/com/test/RestartingProxyTest.java
+++ littleproxy-test/src/main/java/com/test/RestartingProxyTest.java
@@ -3,24 +3,21 @@ package com.test;
 import org.littleshoot.proxy.HttpProxyServer;
 import org.littleshoot.proxy.impl.DefaultHttpProxyServer;
 
-/**
- * Hello world!
- *
- */
 public class RestartingProxyTest 
 {
     public static void main( String[] args )
     {
      
     	for (int i=1; i<25; i++){
-    		System.out.println("Start-stop proxy. Iteration: "+i);
+    		System.out.println("Start proxy. Iteration: "+i);
     		HttpProxyServer server =
         		    DefaultHttpProxyServer.bootstrap()
         		        .withPort(8080)
         		        .start();
+    		System.out.println("Proxy started. Stopping proxy. Iteration: "+i);
         	server.stop();
+        	System.out.println("Proxy stopped. Iteration: "+i);
     	}
-  	
     	
     	System.out.println( "Done!" );
     }
